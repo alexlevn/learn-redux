5863f82ddfa2ff6712b2c26e4c026968d034d13c
thanhdungfb <thanhdungfb@gmail.com>
2018-04-18 16:33:02 +0700

    add support for change name action

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,44 +2,43 @@ var redux = require('redux');
 
 console.log('Starting redux example');
 
-// Pure function
-function add(a, b) {
-    return a + b;
+// reducer: là một hàm, được tuyền vào 2 biến: trạng thái state và hành động action
+// Ê, giảm tốc xuống, để tao dễ quản lý
+var reducer = (state = {name: 'Anonymous'}, action) => {
+    // state = state || {name: 'Anonymous'}
+    console.log('Hành động sẽ được gọi: ', action);
+
+    // Lựa chọn hành động để gọi:
+    switch (action.type) {
+        // Đổi tên hả, uh, tao sẽ đổi tên của state
+        case 'CHANGE_NAME':
+            return {
+                ...state,
+                name: action.name
+            };
+        // Mặc định: Không làm gì nha
+        default:
+            return state;
+    }
 }
 
-var a = 3;
-function add(b) {
-    return a + b;
-}
+// Tạo một của hàng. Làm theo kiểu reducer ở bên trên. 
+var store = redux.createStore(reducer);
 
-var result;
-function add(a, b) {
-    result = a + b;
-    return result;
-}
+// Chưa làm gì. Lấy trạng thái ra xem sao
+var currentState = store.getState();
 
-function add(a, b) {
-    return a + b + new Date().getSeconds();
-}
+// Giá trị được lấy nè mày. 
+console.log('currentState: ', currentState);
 
-function changProp(obj) {
-    return {
-        ...obj,
-        name: 'Jens',
-        isStudent: true
-    };
-    //git obj.name = 'Jens'; 
-    // obj.isStudent = true;
+// Giờ tao thích truyền đối tượng kiểu khác à. Kiểu đó tao sẽ lưu trong
+// state đó nhé.
+// Ủa, vậy action.type là type trong state đó hả. 
+// dispatch là: gửi vào cho mày thanh toán, tao chỉ biết gửi thôi,
+// Không biết xử lý cái quái gì đó
+store.dispatch({
+    type: 'CHANGE_NAME',
+    name: 'Andrew'
+});
 
-    // return obj;
-}
-
-var startingValue = {
-    name: "White",
-    age: 30
-};
-
-var res = changProp(startingValue);
-
-console.log(startingValue);
-console.log(res)
+console.log('Name should be Andree', store.getState());
