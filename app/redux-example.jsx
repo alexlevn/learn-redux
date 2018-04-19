9e5c134f1bfd3e142dfe43baa8e169ec03bbc115
thanhdungfb <thanhdungfb@gmail.com>
2018-04-19 10:14:35 +0700

    add action for adding/removing hobby and movie

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,67 +2,97 @@ var redux = require('redux');
 
 console.log('Starting redux example');
 
-// reducer: là một hàm, được tuyền vào 2 biến: trạng thái state có giá trị defaulte 
-// và hành động action (định nghĩa là obj, dựa vào đó để action..)
-// Ê, giảm tốc xuống, để tao dễ quản lý
-var reducer = (state = {name: 'Anonymous'}, action) => {
-    // state = state || {name: 'Anonymous'}
-    // console.log('Hành động sẽ được gọi: ', action);
+var stateDefault = {
+    name: 'Anonymous',
+    hobbies: [],
+    movies: []
+}
 
-    // Lựa chọn hành động để gọi:
+var nextHobbyId = 1;
+var nextMovieId = 1;
+
+var reducer = (state = stateDefault, action) => {
     switch (action.type) {
-        // Đổi tên hả, uh, tao sẽ đổi tên của state
         case 'CHANGE_NAME':
             return {
                 ...state,
                 name: action.name
             };
-        // Mặc định: Không làm gì nha
+        case 'ADD_HOBBY':
+            return {
+                ...state,
+                hobbies: [
+                    ...state.hobbies, {
+                        id: nextHobbyId++,
+                        hobby: action.hobby
+                    }
+                ]
+            };
+        case 'REMOVE_HOBBY':
+            // debugger;
+            return {
+                ...state,
+                hobbies: state
+                    .hobbies
+                    .filter((hobby) => (hobby.id !== action.id))
+            }
+        case 'ADD_MOVIE':
+            return {
+                ...state,
+                movies: [
+                    ...state.movies, {
+                        id: nextMovieId++,
+                        title: action.title,
+                        genre: action.genre
+                    }
+                ]
+            }
+        case 'REMOVE_MOVIE':
+            return {
+                ...state,
+                movies: state
+                    .movies
+                    .filter((movie) => movie.id !== action.id)
+            }
         default:
             return state;
     }
 }
-
-// Tạo một của hàng. Làm theo kiểu reducer ở bên trên. 
-var store = redux.createStore(reducer, redux.compose(
-    window.devToolsExtension ? window.devToolsExtension() : (f)=>{
+var store = redux.createStore(reducer, redux.compose(window.devToolsExtension
+    ? window.devToolsExtension()
+    : (f) => {
         return f;
-    }
-));
+    }));
 
-// Subscribe to changes
-// Mỗi lần dispatch sẽ gọi subcribe này 
-var unsubcribe = store.subscribe(()=>{
+var unsubcribe = store.subscribe(() => {
     var state = store.getState();
 
     console.log('Name is ', state.name);
 
-    document.getElementById('app').innerHTML = state.name;
+    document
+        .getElementById('app')
+        .innerHTML = state.name;
+
+    console.log('New state', store.getState());
 
 });
 // unsubcribe();
-// Khóa lại, không cho thực thi nữa.???
-
-// Chưa làm gì. Lấy trạng thái ra xem sao
 var currentState = store.getState();
 
-// Giá trị được lấy nè mày. 
 console.log('currentState: ', currentState);
 
-// Giờ tao thích truyền đối tượng kiểu khác à. Kiểu đó tao sẽ lưu trong
-// action đó nhé.
-// Ủa, vậy action.type là type trong đó hả: Không phải, nó là action đó
-// dispatch là: gửi vào cho mày thanh toán, tao chỉ biết gửi thôi,
-// Không biết xử lý cái quái gì đó. Khi gửi, nhớ gửi tham số đi kèm vào action.
-store.dispatch({
-    type: 'CHANGE_NAME',
-    name: 'Andrew'
-});
+store.dispatch({type: 'CHANGE_NAME', name: 'Andrew'});
 
+store.dispatch({type: "ADD_HOBBY", hobby: "running"});
 
-store.dispatch({
-    type: 'CHANGE_NAME',
-    name: 'Emily'
-})
+store.dispatch({type: "ADD_HOBBY", hobby: "walking"});
+
+store.dispatch({type: 'REMOVE_HOBBY', id: 2});
+
+store.dispatch({type: 'CHANGE_NAME', name: 'Emily'})
+
+store.dispatch({type: 'ADD_MOVIE', name: 'Mad Max', genre: 'Action'});
+store.dispatch({type: 'ADD_MOVIE', name: 'Fast and Furious', genre: 'affection'});
+store.dispatch({type: 'REMOVE_MOVIE', id: 2});
 
 // console.log('Name should be Andree', store.getState());
--- node.md
+++ node.md
@@ -1,3 +1,14 @@
 Some notes for newbie:
 
 - imports get sorted to the top of the file, and requires stay where they were put. So the run-order only changes with import.
+
+// Remove item from array
+// filter: => brower every elements and check with callback function.
+- array.filter(function(item){
+    return (item.id !== action.id);
+    // return true: => keep
+    // return false: => remove
+})
+
+With arrow func:
+hobbies: state.hobbies.filter((hobby)=>{hobby.id !== action.id})
