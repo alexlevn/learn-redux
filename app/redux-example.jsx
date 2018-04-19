996b87b1641f23a0b39a82b2060be2ef705127d2
thanhdungfb <thanhdungfb@gmail.com>
2018-04-19 11:34:17 +0700

    break up reducer into multiple reducers

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -11,7 +11,8 @@ var stateDefault = {
 var nextHobbyId = 1;
 var nextMovieId = 1;
 
-var reducer = (state = stateDefault, action) => {
+// The first way: dont use it anymore
+var oldReducer = (state = stateDefault, action) => {
     switch (action.type) {
         case 'CHANGE_NAME':
             return {
@@ -58,6 +59,57 @@ var reducer = (state = stateDefault, action) => {
             return state;
     }
 }
+
+// The second way: 
+var nameReducer = (state = "Anonymous", action) => {
+    switch (action.type) {
+        case 'CHANGE_NAME':
+            return action.name;
+
+        default:
+            return state;
+    };
+};
+
+var hobbiesReducer = (state = [], action) => {
+    switch (action.type) {
+        case 'ADD_HOBBY':
+            return [
+                ...state, {
+                    id: nextHobbyId++,
+                    hobby: action.hobby
+                }
+            ];
+        case 'REMOVE_HOBBY':
+            return state.filter((hobby) => hobby.id !== action.id);
+        default:
+            return state;
+    }
+};
+
+var moviesReducer = function (state = [], action) {
+    switch (action.type) {
+        case "ADD_MOVIE":
+            return [
+                ...state, {
+                    id: nextMovieId++,
+                    title: action.title,
+                    genre: action.genre
+                }
+            ]
+        case "REMOVE_MOVIE":
+            return state.filter((movie) => (movie.id !== action.id));
+        default:
+            return state
+    }
+};
+
+var reducer = redux.combineReducers({
+    name: nameReducer, 
+    hobbies: hobbiesReducer, 
+    movies: moviesReducer
+});
+
 var store = redux.createStore(reducer, redux.compose(window.devToolsExtension
     ? window.devToolsExtension()
     : (f) => {
@@ -92,7 +144,7 @@ store.dispatch({type: 'REMOVE_HOBBY', id: 2});
 store.dispatch({type: 'CHANGE_NAME', name: 'Emily'})
 
 store.dispatch({type: 'ADD_MOVIE', name: 'Mad Max', genre: 'Action'});
-store.dispatch({type: 'ADD_MOVIE', name: 'Fast and Furious', genre: 'affection'});
-store.dispatch({type: 'REMOVE_MOVIE', id: 2});
 
-// console.log('Name should be Andree', store.getState());
+store.dispatch({type: 'ADD_MOVIE', name: 'Fast and Furious', genre: 'affection'});
+
+store.dispatch({type: 'REMOVE_MOVIE', id: 2});
